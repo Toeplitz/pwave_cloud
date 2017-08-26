eaad1a7a12006c2cde9ee7827efa486b19d1c514
Martin Sarajaervi <balony@gmail.com>
2017-08-26 21:14:46 +0200

    sync

--- pwave_client_async.cpp
+++ pwave_client_async.cpp
@@ -1,38 +1,6 @@
-/*
- *
- * Copyright 2015, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
 #include <iostream>
 #include <memory>
+#include <chrono>
 #include <string>
 
 #include <grpc++/grpc++.h>
@@ -49,6 +17,7 @@ using grpc::Status;
 using pwave::HelloRequest;
 using pwave::HelloReply;
 using pwave::Greeter;
+
 
 class GreeterClient {
   public:
@@ -143,7 +112,9 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < 1000; i++) {
         std::string user("world " + std::to_string(i));
+        printf("sending call %d\n", i);
         greeter.SayHello(user);  // The actual RPC call!
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     std::cout << "Press control-c to quit" << std::endl << std::endl;
