d8222c3ca623e4ab420105d923f5287ef111ae93
Martin Sarajaervi <balony@gmail.com>
2017-08-26 21:31:30 +0200

    sync

--- pwave_client_async.cpp
+++ pwave_client_async.cpp
@@ -114,7 +114,7 @@ int main(int argc, char** argv) {
         std::string user("world " + std::to_string(i));
         printf("sending call %d\n", i);
         greeter.SayHello(user);  // The actual RPC call!
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
     std::cout << "Press control-c to quit" << std::endl << std::endl;
